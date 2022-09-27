62d89e7b81e2b00d39929bce953a4eefa0fb0b93
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-09-27 15:34:04 +0200

    created some issues and parented them

--- src/logme.js
+++ src/logme.js
@@ -1,2 +1,12 @@
 console.log('me');
 // TODO #1 how can someone create an issue on github???
+// TODO #2 first create the small todos
+// TODO #3 like a little thing 1
+// TODO #4 a little thing 2
+// TODO #5 then merge every little todo when you edit the yaml
+// file in the code editore opened when editing the title of
+// the issue (pressing the edit button when the application
+// asks to inster the title, opens the text editor to
+// manipulate the yaml file which is used by github to
+// create the issue.)
+

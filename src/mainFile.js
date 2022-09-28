4439d8ed7a2cbb54599674a2c496edb874804bc7
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-09-28 11:17:35 +0200

    do it

--- src/mainFile.js
+++ src/mainFile.js
@@ -1,2 +1,2 @@
-console.log('I am working on a new feature ... and I also changed a file that you should also change.');
+console.log('I am working on the main ... and I also changed a file that you should also change.');
 console.log('lets change together the file called change 1');
